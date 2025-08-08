61372b1de43a24a85d1789ecaea09d004165597f
narendraKodeX <narendrakat786@gmail.com>
2025-08-09 01:25:17 +0545

    update view_student feature and fix error

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ students = {}
 
 #function to add a student to the dictionary
 def add_student(student_name,subject, marks):
+    if student_name not in students:
+        students[student_name] = {}
     students[student_name][subject] = int(marks)
     print(f"Added subject {subject} with marks {marks}")
 
 #Function to view all students
-def view_students():
-    pass
+def view_students(student_name=None):
+    if students:
+        if student_name in students:
+            for subject , marks in students[student_name].items():
+                print(f"{subject} {marks}")
+
+        else:
+            print("Student not found")
+    else:
+        if not students:
+            print("Studerts not found")
+            return
+        for student, subject in students.itmes():
+            print(f"\nstudent {student}")
+
+            for subject , marks in students.items():
+                print(f"{subject} {marks}")
 
 def main():
     while True:
@@ -17,23 +34,28 @@ def main():
         print("2. View Students")
         print("3. Exit")
 
-        #choice the user
+
         choice = input("Enter your choice: \n")
 
         if choice == '1':
             student_name = input("Enter student name: \n")
             subject = input("Enter subject name: \n")
             marks = input("Enter student marks: \n")
-            add_student(student_name,subject,marks)
+            add_student(student_name, subject, marks)
 
-        elif choice =='2':
-            view_students()
+        elif choice == '2':
+            name = input("Enter student name to view (leave empty for all): \n")
+            if name.strip() == "":
+                view_students()
+            else:
+                view_students(name.strip())
 
-        elif choice =='3':
+        elif choice == '3':
             print("Goodbye!!")
             break
 
-            
+        else:
+            print("Invalid choice, try again.")
 
 
 main()
