205c2e48dfabc7f51e0fe6f97869b5021fca7775
narendraKodeX <narendrakat786@gmail.com>
2025-08-09 00:02:48 +0545

    Fixed input choice comparison to strings

--- main.py
+++ main.py
@@ -19,15 +19,16 @@ def main():
         #choice the user
         choice = input("Enter your choice: \n")
 
-        if choice == 1:
+        if choice == '1':
             name = input("Enter student name: \n")
             marks = input("Enter student marks: \n")
             add_student(name,marks)
 
-        elif choice ==2:
+        elif choice =='2':
             view_students()
 
-        elif choice ==3:
+        elif choice =='3':
+            print("Goodbye!!")
             break
 
             
